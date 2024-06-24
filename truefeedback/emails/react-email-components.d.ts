78842d3c49e5e9a6c13ab96278bbba3df4eae5bb
devanshV18 <devanshverma024@gmail.com>
2024-06-24 17:05:56 +0530

    imported template email

---
+++ truefeedback/emails/react-email-components.d.ts
@@ -0,0 +1,13 @@
+// src/react-email-components.d.ts or emails/react-email-components.d.ts
+declare module '@react-email/components' {
+    export const Html: any;
+    export const Head: any;
+    export const Font: any;
+    export const Preview: any;
+    export const Heading: any;
+    export const Row: any;
+    export const Section: any;
+    export const Text: any;
+    export const Button: any;
+  }
+  
